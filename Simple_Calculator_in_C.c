2eb152861df6fa7926628bb4d77a61ce9f07d78f
mahamud-13756 <mahamud15-13756@diu.edu.bd>
2021-02-26 23:14:25 +0600

    add sign and take input function

--- Simple_Calculator_in_C.c
+++ Simple_Calculator_in_C.c
@@ -4,4 +4,11 @@
      char ch;
      int num1,num2;
 
+     printf("Choose the operator(+,-,*,/,%%): ");
+     scanf("%c",&ch);
+
+     printf("Enter two numbers: ");
+     scanf("%d %d",&num1,&num2);
+
+
  }
